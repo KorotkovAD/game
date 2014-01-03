b2f24c049ce437d1ad3ddc4edd6af341486927a6
Anton Korotkov <anton.korotkov@cyoda.com>
2014-01-03 18:39:16 +0400

    Removed sqlite3 from Gemfile

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,12 +1,7 @@
 # Be sure to restart your server when you modify this file.
 
-# Your secret key is used for verifying the integrity of signed cookies.
+# Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-# You can use `rake secret` to generate a secure secret key.
-
-# Make sure your secret_key_base is kept private
-# if you're sharing your code publicly.
-Game::Application.config.secret_key_base = '40547446ca456f7063daa9d5aad1e96764cb1d3233489101a99d39ac4e7e30bcfb5ff89d5ee76a175a7c92d5657d1dd5769aaa9d5029a717b6b83428241e4743'
+Game::Application.config.secret_token = 'ed8a3594f31478758876b3e729086d5b629fc9daca0eb69688ce7407e211b51a9fa738378b0f946dafef1dbdd5185c582dbf4debc59dfe9af30cf603282f717d'
